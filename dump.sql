4b15445d61540f623817f37d8d1430ebcfc0ad8e
Karina <karinaalmeida523@gmail.com>
2023-02-28 14:27:36 -0300

    dump3

--- dump.sql
+++ dump.sql
@@ -21,7 +21,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: sessions; Type: TABLE; Schema: public; Owner: postgres
+-- Name: sessions; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.sessions (
@@ -32,10 +32,8 @@ CREATE TABLE public.sessions (
 );
 
 
-ALTER TABLE public.sessions OWNER TO postgres;
-
 --
--- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.sessions_id_seq
@@ -47,17 +45,15 @@ CREATE SEQUENCE public.sessions_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.sessions_id_seq OWNER TO postgres;
-
 --
--- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
 
 
 --
--- Name: url; Type: TABLE; Schema: public; Owner: postgres
+-- Name: url; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.url (
@@ -70,10 +66,8 @@ CREATE TABLE public.url (
 );
 
 
-ALTER TABLE public.url OWNER TO postgres;
-
 --
--- Name: url_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: url_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.url_id_seq
@@ -85,17 +79,15 @@ CREATE SEQUENCE public.url_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.url_id_seq OWNER TO postgres;
-
 --
--- Name: url_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: url_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.url_id_seq OWNED BY public.url.id;
 
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
+-- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.users (
@@ -107,10 +99,8 @@ CREATE TABLE public.users (
 );
 
 
-ALTER TABLE public.users OWNER TO postgres;
-
 --
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.users_id_seq
@@ -122,90 +112,82 @@ CREATE SEQUENCE public.users_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.users_id_seq OWNER TO postgres;
-
 --
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- Name: sessions id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: sessions id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.sessions_id_seq'::regclass);
 
 
 --
--- Name: url id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: url id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.url ALTER COLUMN id SET DEFAULT nextval('public.url_id_seq'::regclass);
 
 
 --
--- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
 
 --
--- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.sessions (id, "userId", "userToken", "createdAt") FROM stdin;
-4	7	81eeb5a5-03a1-4c16-a72a-0cae1908bb35	2023-02-28 13:34:28.252023
-\.
+INSERT INTO public.sessions VALUES (4, 7, '81eeb5a5-03a1-4c16-a72a-0cae1908bb35', '2023-02-28 13:34:28.252023');
 
 
 --
--- Data for Name: url; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: url; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.url (id, "userId", url, short, "visitCount", "createdAt") FROM stdin;
-\.
 
 
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.users (id, name, email, password, "createdAt") FROM stdin;
-1	Karina	karina@email.com	supersenha1234	2023-02-27 13:48:17.754711
-2	Will	will@email.com	$2b$10$wya6761F54aT3yCRHxFE1eOz8myaCJx1.IGHVTixFCk.eOu6kjxpu	2023-02-27 15:25:33.398091
-3	maria	maria@email.com	$2b$10$hT6tw10woVXO1P23LA4ny.tSUZL/nLwf9GnufJXPomcfwTUaIylTG	2023-02-27 15:27:39.005901
-5	joe	joe@email.com	$2b$10$VnB2415YSuuijvAr2s9GCOeoGY3j6ZNj2gB12IIjcZfIBqqT9pUPK	2023-02-27 15:46:00.494659
-6	larissa	larissa@email.com	$2b$10$Ftz1dkwOm.ljR85XBvowiuxbD9trS1sQmq87rAumyQLvtb2gdFFta	2023-02-27 15:50:28.105363
-7	marcos	marcos@email.com	$2b$10$ey5BfTCJnn49MRdTeGDfX.150fn9Slsi.N/fP9FjdJHFONkugo2q6	2023-02-27 15:58:36.792613
-\.
+INSERT INTO public.users VALUES (1, 'Karina', 'karina@email.com', 'supersenha1234', '2023-02-27 13:48:17.754711');
+INSERT INTO public.users VALUES (2, 'Will', 'will@email.com', '$2b$10$wya6761F54aT3yCRHxFE1eOz8myaCJx1.IGHVTixFCk.eOu6kjxpu', '2023-02-27 15:25:33.398091');
+INSERT INTO public.users VALUES (3, 'maria', 'maria@email.com', '$2b$10$hT6tw10woVXO1P23LA4ny.tSUZL/nLwf9GnufJXPomcfwTUaIylTG', '2023-02-27 15:27:39.005901');
+INSERT INTO public.users VALUES (5, 'joe', 'joe@email.com', '$2b$10$VnB2415YSuuijvAr2s9GCOeoGY3j6ZNj2gB12IIjcZfIBqqT9pUPK', '2023-02-27 15:46:00.494659');
+INSERT INTO public.users VALUES (6, 'larissa', 'larissa@email.com', '$2b$10$Ftz1dkwOm.ljR85XBvowiuxbD9trS1sQmq87rAumyQLvtb2gdFFta', '2023-02-27 15:50:28.105363');
+INSERT INTO public.users VALUES (7, 'marcos', 'marcos@email.com', '$2b$10$ey5BfTCJnn49MRdTeGDfX.150fn9Slsi.N/fP9FjdJHFONkugo2q6', '2023-02-27 15:58:36.792613');
 
 
 --
--- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
 SELECT pg_catalog.setval('public.sessions_id_seq', 4, true);
 
 
 --
--- Name: url_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: url_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
 SELECT pg_catalog.setval('public.url_id_seq', 1, false);
 
 
 --
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
 SELECT pg_catalog.setval('public.users_id_seq', 7, true);
 
 
 --
--- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.sessions
@@ -213,7 +195,7 @@ ALTER TABLE ONLY public.sessions
 
 
 --
--- Name: sessions sessions_userToken_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: sessions sessions_userToken_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.sessions
@@ -221,7 +203,7 @@ ALTER TABLE ONLY public.sessions
 
 
 --
--- Name: url url_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: url url_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.url
@@ -229,7 +211,7 @@ ALTER TABLE ONLY public.url
 
 
 --
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users
@@ -237,7 +219,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users
@@ -245,7 +227,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: sessions sessions_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: sessions sessions_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.sessions
@@ -253,7 +235,7 @@ ALTER TABLE ONLY public.sessions
 
 
 --
--- Name: url url_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Name: url url_userId_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.url
