f3f706ae7e8824bcb2b87e1e8a3cb933a64b8252
Karina <karinaalmeida523@gmail.com>
2023-02-28 14:32:41 -0300

    bugfix

--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -3,7 +3,7 @@ export default function validateLogin (loginSchema) {
     return (req, res, next)=> {
        
         const {error}= loginSchema.validate (req.body, {abortEarly: false})
-        if (error) return res.status(400).send(error.message)  
+        if (error) return res.status(422).send(error.message)  
 
         next();
     }
--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -3,7 +3,7 @@ export default function validateUser (userSchema) {
     return (req, res, next)=> {
        
         const {error}= userSchema.validate (req.body, {abortEarly: false})
-        if (error) return res.status(400).send(error.message)  
+        if (error) return res.status(422).send(error.message)  
 
         next();
     }
